852352173a894ef1be719daba7e00dfe76b2917b
Edegare <114100784+Edegare@users.noreply.github.com>
2024-04-22 23:17:01 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 //javac -d bin src/*.java src/users/*.java src/activities/*.java 
+//java -cp bin Main
 
 public class Main {
     public static void main(String[] args) throws Exception {
